a6d722f4eef8eabd0febf6cbda0855c1d52b9448
anjanshrestha8 <anjanshrestha2023@gmail.com>
2024-08-14 13:36:20 +0545

    day 1 added

---
+++ Day-1/script.js
@@ -0,0 +1,19 @@
+gsap.to("#box1", {
+  x: 500,
+  y: 500,
+  duration: 2,
+  delay: 1,
+});
+
+gsap.to("#box2", {
+  x: 500,
+  duration: 2,
+  delay: 1,
+});
+
+gsap.to("#box3", {
+  y: -500,
+  x: 500,
+  duration: 2,
+  delay: 1,
+});
